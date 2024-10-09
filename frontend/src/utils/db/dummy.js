37f669b247a0545af63ff6146b5dd84452267995
Adik Soleh <adiksoleh4@gmail.com>
2024-10-10 02:22:15 +0700

    UI Design with daisyui

---
+++ frontend/src/utils/db/dummy.js
@@ -0,0 +1,116 @@
+export const POSTS = [
+	{
+		_id: "1",
+		text: "Deg degan oy 21 hari lagi 😍",
+		img: "/posts/ayang1.JPG",
+		user: {
+			username: "dewism",
+			profileImg: "/avatars/girl1.png",
+			fullName: "Dewi Sri",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "Kelas mang",
+				user: {
+					username: "paijo",
+					profileImg: "/avatars/girl1.png",
+					fullName: "Paijo",
+				},
+			},
+		],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+	},
+	{
+		_id: "2",
+		text: "Apa Kabar Hari Ini? 😊",
+		user: {
+			username: "opet",
+			profileImg: "/avatars/boy2.png",
+			fullName: "Apin",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "Bae mas",
+				user: {
+					username: "parjo",
+					profileImg: "/avatars/girl2.png",
+					fullName: "Parjo Js",
+				},
+			},
+		],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+	},
+	{
+		_id: "3",
+		text: "Terbang ke angkasa biar jauh. 🚀",
+		img: "/posts/post2.png",
+		user: {
+			username: "adik.s",
+			profileImg: "/avatars/boy3.png",
+			fullName: "Adi S",
+		},
+		comments: [],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894", "6658s895", "6658s896"],
+	},
+	{
+		_id: "4",
+		text: "Komen yang Mau belajar ini? 🤔",
+		img: "/posts/post3.png",
+		user: {
+			username: "johndoe",
+			profileImg: "/avatars/boy3.png",
+			fullName: "John Doe",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "Bole bang tutor",
+				user: {
+					username: "janedoe",
+					profileImg: "/avatars/girl3.png",
+					fullName: "Jane Doe",
+				},
+			},
+		],
+		likes: [
+			"6658s891",
+			"6658s892",
+			"6658s893",
+			"6658s894",
+			"6658s895",
+			"6658s896",
+			"6658s897",
+			"6658s898",
+			"6658s899",
+		],
+	},
+];
+
+export const USERS_FOR_RIGHT_PANEL = [
+	{
+		_id: "1",
+		fullName: "Dewi Sri",
+		username: "DewiSm",
+		profileImg: "/avatars/boy2.png",
+	},
+	{
+		_id: "2",
+		fullName: "Opin",
+		username: "opet",
+		profileImg: "/avatars/girl1.png",
+	},
+	{
+		_id: "3",
+		fullName: "Adi",
+		username: "adi",
+		profileImg: "/avatars/boy3.png",
+	},
+	{
+		_id: "4",
+		fullName: "Caca",
+		username: "caca",
+		profileImg: "/avatars/girl2.png",
+	},
+];
